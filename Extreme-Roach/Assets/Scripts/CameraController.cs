fa8b2e7715edb52729f0b335e078ab6fdf803901
Strikingletsplays <007makiz@gmail.com>
2018-10-21 21:05:18 +0300

    Movement + Collision

--- Extreme-Roach/Assets/Scripts/CameraController.cs
+++ Extreme-Roach/Assets/Scripts/CameraController.cs
@@ -15,7 +15,7 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        targetPos = new Vector3(followTarget.transform.position.x - 15.5f, followTarget.transform.position.y - 15.5f, followTarget.transform.position.z -10f);
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, followTarget.transform.position.z -10f);
         transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
 	}
 }
