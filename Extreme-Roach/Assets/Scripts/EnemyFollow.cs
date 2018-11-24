f31e94cfdfc4ba65848c0eacb5052954c7b8a340
Strikingletsplays <007makiz@gmail.com>
2018-11-24 15:40:57 +0200

    Enemy Movement (kinda)

---
+++ Extreme-Roach/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D myRigidbody;
+    private bool moving;
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+    public float timeToMove;
+    private float timeToMoveCounter;
+    private Vector3 moveDirection;
+
+    private Transform target;
+    private Player player;
+
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+        timeBetweenMoveCounter = timeBetweenMove;
+        timeToMoveCounter = timeToMove;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //Roach dies
+        if(Vector2.Distance(transform.position, target.position) < 3)
+        {
+            moving = true;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+        //
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = moveDirection;
+
+            if(timeBetweenMoveCounter > 0)
+            {
+                moving = false;
+                timeBetweenMoveCounter = timeBetweenMove;
+            }
+                
+        }
+        else
+        {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = Vector2.zero;
+            if(timeBetweenMoveCounter< 0f)
+            {
+                moving = true;
+                timeToMoveCounter = timeToMove;
+
+                moveDirection = new Vector3(Random.Range(-1f, 1f) * speed, Random.Range(-1f, 1f) * speed, 0f);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.helth--;
+
+        }
+    }
+}
---
+++ Extreme-Roach/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public int helth = 2;
+    public Text helthDisplay;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        helthDisplay.text = "Helth : " + helth;
+
+		if(helth <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
