62d180e7ebae28c36119c14898beca008bf870fb
Strikingletsplays <007makiz@gmail.com>
2018-11-25 12:59:18 +0200

    Fixed Player dies and restarts leavel

--- Extreme-Roach/Assets/Scripts/EnemyFollow.cs
+++ Extreme-Roach/Assets/Scripts/EnemyFollow.cs
@@ -1,76 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour {
 
     public float speed;
     public float radius = 1f;
-
-    private Rigidbody2D myRigidbody;
-    private bool moving;
-    public float timeBetweenMove;
-    private float timeBetweenMoveCounter;
-    public float timeToMove;
-    private float timeToMoveCounter;
-    private Vector3 moveDirection;
-
-    private Transform target;
+    private Transform playerPos;
     private Player player;
 
     // Use this for initialization
     void Start () {
-        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
-        player = GameObject.FindWithTag("Player").GetComponent<Player>();
-
-        myRigidbody = GetComponent<Rigidbody2D>();
-        timeBetweenMoveCounter = timeBetweenMove;
-        timeToMoveCounter = timeToMove;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         
         //Roach dies
-        if(Vector2.Distance(transform.position, target.position) < radius)
+        if(Vector2.Distance(transform.position, playerPos.position) < radius)
         {
-            moving = true;
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
         }
-
-        //
- //       if (moving)
- //       {
- //           timetomovecounter -= time.deltatime;
- //           myrigidbody.velocity = movedirection;
-//
- //           if(timebetweenmovecounter > 0)
- //           {
- //              moving = false;
- //               timebetweenmovecounter = timebetweenmove;
- //           }
- //               
- //       }
- //       else
- //        {
- //           timebetweenmovecounter -= time.deltatime;
- //           myrigidbody.velocity = vector2.zero;
- //           if(timebetweenmovecounter< 0f)
- //           {
- //               moving = true;
- //               timetomovecounter = timetomove;
-                
- //               movedirection = new vector3(random.range(-1f, 1f) * speed, random.range(-1f, 1f) * speed, 0f);
- //           }
- //       }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other )
     {
         if (other.CompareTag("Player"))
         {
+            //player lossing helth
             player.helth--;
-
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
--- Extreme-Roach/Assets/Scripts/Player.cs
+++ Extreme-Roach/Assets/Scripts/Player.cs
@@ -6,23 +6,15 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-    public int helth = 2;
-    public Text helthDisplay;
-
+    public int helth = 1;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        helthDisplay.text = "Helth : " + helth;
-
-		if(helth <= 0)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
 	}
 }
