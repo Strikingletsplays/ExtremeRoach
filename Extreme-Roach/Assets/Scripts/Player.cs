df36a61a2a31fcf8c0c7dd9eff367e99a5f84b3f
StrikingLetsPlays <007makiz@gmail.com>
2018-12-10 14:55:35 +0200

    Changes

--- Extreme-Roach/Assets/Scripts/Player.cs
+++ Extreme-Roach/Assets/Scripts/Player.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
+    private Player player;
     public int health = 3;
     public Text healthText;
+    public Text into;
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         healthText.text = "Health: " + health;
 	}
+
 }
