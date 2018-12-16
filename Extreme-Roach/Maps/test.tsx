f6b55537a69dda868259da9f407a2af42947c261
StrikingLetsPlays <007makiz@gmail.com>
2018-12-16 17:10:45 +0200

    restart

--- Extreme-Roach/Maps/test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Main Tile set" tilewidth="16" tileheight="16" spacing="1" tilecount="7744" columns="88">
- <image source="../Assets/Art/Vector_set.png" width="1500" height="1500"/>
-</tileset>
