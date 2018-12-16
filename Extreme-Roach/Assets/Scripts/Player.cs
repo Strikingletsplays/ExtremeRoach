e0b6ca5767002d384381945cb124df63fcf582cd
MariosKamis <marios.kaminarides@hotmail.com>
2018-12-16 19:49:59 +0200

    health system finished

--- Extreme-Roach/Assets/Scripts/Player.cs
+++ Extreme-Roach/Assets/Scripts/Player.cs
@@ -7,18 +7,48 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour {
 
     private Player player;
+    public GameObject health1, health2, health3;
     public int health = 3;
     public Text healthText;
     public Text into;
 
     // Use this for initialization
     void Start () {
+        health1.gameObject.SetActive(true);
+        health2.gameObject.SetActive(true);
+        health3.gameObject.SetActive(true);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        healthText.text = "Health: " + health;
-	}
+       // healthText.text = "Health: " + health;
 
+        switch (health)
+        {
+            case 3:
+                health1.gameObject.SetActive(true);
+                health2.gameObject.SetActive(true);
+                health3.gameObject.SetActive(true);
+                break;
+            case 2:
+                health1.gameObject.SetActive(true);
+                health2.gameObject.SetActive(true);
+                health3.gameObject.SetActive(false);
+                break;
+            case 1:
+                health1.gameObject.SetActive(true);
+                health2.gameObject.SetActive(false);
+                health3.gameObject.SetActive(false);
+                break;
+            case 0:
+                health1.gameObject.SetActive(false);
+                health2.gameObject.SetActive(false);
+                health3.gameObject.SetActive(false);
+               // gameOver.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                break;
+        }
+    }
+    
 }
